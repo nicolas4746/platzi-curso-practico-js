aefd4ab06e2e87c754a6d8874988040fafccd135
Nicolas Goitia <nicolasgoitia10@gmail.com>
2021-12-16 20:37:13 -0500

    empezamos el taller 2

---
+++ taller 2/descuentos.js
@@ -0,0 +1,33 @@
+/* const precioOriginal = 120;
+const descuento = 18; */
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento =
+    100 - descuento;
+    const precioConDescuento = 
+    (precio * porcentajePrecioConDescuento) / 100;
+    
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+   const inputPrice = document.getElementById("inputPrice"); 
+    const priceValue = inputPrice.value;
+
+
+   const inputDiscount = document.getElementById("inputDiscount"); 
+   const discountValue = inputDiscount.value;
+
+   const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+   const resultP = document.getElementById("ResultP");
+   resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
+
+
+/* console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento,
+}); */
